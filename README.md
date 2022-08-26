76f25faef826dd1c8d95257da71d9e62b62931d9
Yavuz Özbay <ozbayavz@gmail.com>
2022-08-26 18:20:44 +0200

    my portfolio with react

---
+++ README.md
@@ -0,0 +1,4 @@
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+3. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
---
+++ src/resumeData.js
@@ -0,0 +1,85 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/yavuzoz",
+    "name": "Yavuz Özbay",
+    "role": "Full stack Developer",
+    "linkedinId":"www.linkedin.com/in/yavuz-özbay-01739b1b1",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Hi I'm 🚀Yavuz Özbay 🚀 I'm a full stack developer, interested in writing new codes, dreaming new designs and creating new web sites but my speciality is front-end developing. I am student at Clarusway and Hi-Coders..",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/yavuz-%C3%B6zbay-01739b1b1/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/yavuzoz",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Curious Full-Stack development, interested in writing new codes, skilled at developing complex solutions, possessing strong creative thinking skills, high energy and integrity. Have the ability to create algorithms effectively, interact positively and communicate appropriately with team members. Quick to grasp new technologies and concepts to develop innovative and creative solutions to problems. Always eager to learn various technologies, tools and libraries. Especially interested in Mobile/Front end/Web Development, React Native, ReactJs, NodeJs, Django and JavaScript. I am excited to learn new things and improve myself.",
+    "address":"Switzerland/Bern",
+    "website":"https://github.com/yavuzoz",
+    "education":[
+      {
+        "UniversityName":"HiCoders Fullstack Programming Participant",
+        "specialization":"Trainee",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2022"
+      },
+      {
+        "UniversityName":"Clarusway Full Stack Developer Programming Participant",
+        "specialization":"Trainee",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020"
+      },
+      {
+        "UniversityName":"Eskisehir Osmangazi University",
+        "specialization":"Bachelor of Sience Fakulty, Chemistry",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2013"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Medbiyo Medicine Company",
+        "specialization":"Chemist",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2019"
+      },
+      {
+        "CompanyName":"Referansis",
+        "specialization":"Human Resources Specialist",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2015"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Mysql"
+      }
+    ],
+  }
+  
+  export default resumeData
